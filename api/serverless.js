caccd91b8227cd07b31614337e88762a820c7cde
GeeKaven <i@tawawa.moe>
2022-03-13 02:02:06 +0800

    fix: fix vercel

--- api/serverless.js
+++ api/serverless.js
@@ -13,7 +13,7 @@ const app = Fastify({
 });
 
 // Register your application as a normal plugin.
-app.register(import("./index.js"));
+app.register(import("./index"));
 
 export default async (req, res) => {
   await app.ready();
