86afcc4b0a48dbea58934a612852062cfd11497b
GeeKaven <i@tawawa.moe>
2022-03-13 16:53:56 +0800

    refactor: split bangumi api and build api

--- app.js
+++ app.js
@@ -1,98 +1,16 @@
 import { fileURLToPath } from 'url'
-import { dirname } from 'path'
-import { tmpdir } from 'os'
-import { existsSync, mkdirSync, writeFileSync, readFileSync } from 'fs'
+import { dirname, join } from 'path'
+import { readFileSync } from 'fs'
 import Cors from 'fastify-cors'
-import axios from 'axios'
-
-const bgmUser = process.env.BANGUMI_USER
-const vercel = process.env.VERCEL_URL
-const bgmUrl = 'https://api.bgm.tv'
-const subjectType = {
-  'anime': 2,
-}
-const collectionType = {
-  'want': 1,       //想看
-  'watched': 2,       //看过
-  'watching': 3,       //在看
-}
-
-const headers = {
-  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
-}
+import Static from 'fastify-static'
 
 const __filename = fileURLToPath(import.meta.url)
-let __dirname = dirname(__filename)
+const __dirname = dirname(__filename)
 
-// 如果部署在 vercel 上，则使用 tmp 目录
-if (vercel) {
-  __dirname = tmpdir()
-}
-
-async function fetchCollection(type) {
-  const url = `${bgmUrl}/v0/users/${bgmUser}/collections`
-
-  try {
-    const { data } = await axios.get(url, {
-      params: {
-        subject_type: subjectType['anime'],
-        type: type,
-        limit: 0,
-        offset: 0
-      },
-      headers: headers
-    })
-    return data
-  } catch (error) {
-    console.log(error)
-    console.log(`[ERROR] Fetch ${url}. error: ${error}`)
-  }
-  return []
-}
-
-async function buildSubject() {
-  // 读取用户番剧收藏
-  const collectionMap = {
-    'want': await fetchCollection(collectionType['want']),
-    'watched': await fetchCollection(collectionType['watched']),
-    'watching': await fetchCollection(collectionType['watching'])
-  }
-
-  for (const key in collectionMap) {
-    if (Object.hasOwnProperty.call(collectionMap, key)) {
-      const collection = collectionMap[key];
-      const data = collection['data']
-      const newData = []
-      for (let i = 0; i < data.length; i++) {
-        const item = data[i];
-        const subjectId = parseInt(item['subject_id'])
-        try {
-          console.log(`- [INFO] Fetch ${key} - ${subjectId}. ${i}/${data.length}`)
-          const { data: subject } = await axios.get(`https://cdn.jsdelivr.net/gh/geekaven/BangumiTV-Subject@latest/data/${Math.floor(subjectId / 100)}/${subjectId}.json`)
-          item['date'] = subject['date']
-          item['images'] = subject['images']
-          item['name'] = subject['name']
-          item['name_cn'] = subject['name_cn']
-          item['summary'] = subject['summary']
-          item['total_episodes'] = subject['total_episodes']
-          item['eps'] = subject['eps']
-          newData.push(item)
-        } catch (error) {
-          console.log(`- [Error] ${key} - ${subjectId}. ${i}/${data.length}`)
-        }
-      }
-
-      const filePath = `${__dirname}/data/${key}.json`
-      const dirName = dirname(filePath)
-      if (!existsSync(dirName)) {
-        mkdirSync(dirName, { recursive: true })
-      }
-      console.log(`- [INFO] Write ${key} to ${filePath}`)
-      writeFileSync(filePath, JSON.stringify(newData))
-    }
-  }
-
-  console.log('- [INFO] Build Subject done.')
+const collectionMap = {
+  'want': [],
+  'watched': [],
+  'watching': [],
 }
 
 export default async function (fastify, opt) {
@@ -101,26 +19,34 @@ export default async function (fastify, opt) {
     origin: true
   })
 
+  fastify.register(Static, {
+    root: join(__dirname, 'public'),
+    prefix: '/public/'
+  })
+
   fastify.get('/', (request, reply) => {
-    reply.send({ hello: 'world' })
+    reply.sendFile('index.html')
   })
 
   fastify.get('/bangumi', (request, reply) => {
     const { type, offset, limit } = request.query
-    const filePath = `${__dirname}/data/${type}.json`
-    if (!existsSync(filePath)) {
-      reply.send({ msg: `No file ${type}` }).statusCode(404)
+
+    if (!collectionMap[type]) {
+      reply.send({ msg: `No collection ${type}` }).statusCode(404)
     }
-    const collection = JSON.parse(readFileSync(filePath))
+
+    const collection = collectionMap[type]
 
     reply.send(collection.slice(offset, offset + limit))
   })
 
-  fastify.get('/build_subject', async (request, reply) => {
-    
-    buildSubject()
-    reply.send({ msg: 'Create build subject job' })
-
+  fastify.addHook('onReady', async () => {
+    for (const key in collectionMap) {
+      if (Object.hasOwnProperty.call(collectionMap, key)) {
+        collectionMap[key] = await JSON.parse(readFileSync(`./data/${key}.json`, 'utf8'))
+      }
+    }
+    console.log(`- [INFO] collection loaded.`)
   })
 }
 
---
+++ collection.js
@@ -0,0 +1,113 @@
+import { dirname } from 'path'
+import { existsSync, mkdirSync, writeFileSync } from 'fs'
+import axios from 'axios'
+
+const bgmUser = process.env.BANGUMI_USER || 'geekaven'
+const bgmUrl = 'https://api.bgm.tv'
+const subjectType = {
+  'anime': 2,
+}
+const collectionType = {
+  'want': 1,       //想看
+  'watched': 2,       //看过
+  'watching': 3,       //在看
+}
+
+const headers = {
+  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+}
+
+async function fetchCollection(limit, offset) {
+  const url = `${bgmUrl}/v0/users/${bgmUser}/collections`
+
+  try {
+    const { data } = await axios.get(url, {
+      params: {
+        subject_type: subjectType['anime'],
+        limit: limit,
+        offset: offset
+      },
+      headers: headers
+    })
+    return data
+  } catch (error) {
+    console.log(`- [ERROR] Fetch ${url}. error: ${error}`)
+  }
+  return false
+}
+
+async function getCollectionMap() {
+
+  const collection = []
+  let limit = 100
+
+  let { total } = await fetchCollection(1, 0)
+  if (!total || total <= 0) {
+    console.log(`- [ERROR] ${bgmUser} no collection.`)
+    return false
+  }
+
+  // 总数不足 limit 时，需修改 limit
+  if (total < limit) {
+    limit = total
+  }
+
+  const totalPage = Math.ceil(total / limit)
+  for (let page = 0; page < totalPage; page++) {
+    console.log(`- [INFO] Fetch ${bgmUser} collection page ${page}/${totalPage}`)
+    const { data } = await fetchCollection(limit, page * limit)
+    if (data) {
+      collection.push(...data)
+    }
+  }
+  
+  const collectionMap = {
+    'want': collection.filter(item => item['type'] === collectionType['want']),
+    'watched': collection.filter(item => item['type'] === collectionType['watched']),
+    'watching': collection.filter(item => item['type'] === collectionType['watching'])
+  }
+
+  return collectionMap
+}
+
+async function buildSubject() {
+  // 读取用户番剧收藏
+  const collectionMap = await getCollectionMap()
+
+  for (const key in collectionMap) {
+    if (Object.hasOwnProperty.call(collectionMap, key)) {
+      const data = collectionMap[key];
+      const newData = []
+      for (let i = 0; i < data.length; i++) {
+        const item = data[i];
+        const subjectId = parseInt(item['subject_id'])
+        try {
+          console.log(`- [INFO] Fetch ${key} - ${subjectId}. ${i}/${data.length}`)
+          const { data: subject } = await axios.get(`https://cdn.jsdelivr.net/gh/geekaven/BangumiTV-Subject@latest/data/${Math.floor(subjectId / 100)}/${subjectId}.json`)
+          item['date'] = subject['date']
+          item['images'] = subject['images']
+          item['name'] = subject['name']
+          item['name_cn'] = subject['name_cn']
+          item['summary'] = subject['summary']
+          item['total_episodes'] = subject['total_episodes']
+          item['eps'] = subject['eps']
+          newData.push(item)
+        } catch (error) {
+          console.log(`- [Error] ${key} - ${subjectId}. ${i}/${data.length}`)
+        }
+      }
+
+      const filePath = `./data/${key}.json`
+      const dirName = dirname(filePath)
+      if (!existsSync(dirName)) {
+        mkdirSync(dirName, { recursive: true })
+      }
+      console.log(`- [INFO] Write ${key} to ${filePath}`)
+      writeFileSync(filePath, JSON.stringify(newData))
+    }
+  }
+
+  console.log('- [INFO] Build Subject done.')
+}
+
+await buildSubject()
