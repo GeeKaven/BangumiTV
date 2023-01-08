08feb389a73cc3ec365c99c6b5601ec5915d834a
GeeKaven <i@tawawa.moe>
2023-01-08 20:54:51 +0800

    feat: add calendar

--- app.js
+++ app.js
@@ -8,15 +8,16 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 const collectionMap = {
-  'want': [],
-  'watched': [],
-  'watching': [],
+  want: [],
+  watched: [],
+  watching: [],
 }
 
-export default async function (fastify, opt) {
+const calendar = []
 
+export default async function (fastify, opt) {
   fastify.register(Cors, {
-    origin: true
+    origin: true,
   })
 
   fastify.register(Static, {
@@ -35,38 +36,61 @@ export default async function (fastify, opt) {
     }
 
     const collection = collectionMap[type]
-    reply.send({data: collection.data.slice(parseInt(offset), parseInt(offset) + parseInt(limit)), total: collection.total})
+    reply.send({
+      data: collection.data.slice(
+        parseInt(offset),
+        parseInt(offset) + parseInt(limit)
+      ),
+      total: collection.total,
+    })
   })
 
   fastify.get('/v2/bangumi', (request, reply) => {
-    const {type, page, size} = request.query
+    const { type, page, size } = request.query
     if (!collectionMap[type]) {
       reply.send({ msg: `No collection ${type}` }).statusCode(404)
     }
 
     const collection = collectionMap[type]
-    reply.send({data: collection.data.slice((parseInt(page) - 1) * size, parseInt(page) * parseInt(size)), total: collection.total})
+    reply.send({
+      data: collection.data.slice(
+        (parseInt(page) - 1) * size,
+        parseInt(page) * parseInt(size)
+      ),
+      total: collection.total,
+    })
   })
 
   fastify.get('/bangumi_total', (request, reply) => {
     const res = {}
     for (const key in collectionMap) {
       if (Object.hasOwnProperty.call(collectionMap, key)) {
-        const collection = collectionMap[key];
+        const collection = collectionMap[key]
         res[key] = collection.total
       }
     }
     reply.send(res)
   })
 
+  fastify.get('/calendar', (request, reply) => {
+    reply.send(calendar)
+  })
+
   fastify.addHook('onReady', async () => {
     for (const key in collectionMap) {
       if (Object.hasOwnProperty.call(collectionMap, key)) {
-        collectionMap[key] = await JSON.parse(readFileSync(`${__dirname}/data/${key}.json`, 'utf8'))
+        collectionMap[key] = await JSON.parse(
+          readFileSync(`${__dirname}/data/${key}.json`, 'utf8')
+        )
       }
     }
+
+    calendar.push(
+      ...(await JSON.parse(
+        readFileSync(`${__dirname}/data/calendar.json`, 'utf-8')
+      ))
+    )
+
     console.log(`- [INFO] collection loaded.`)
   })
 }
-
-
--- collection.js
+++ collection.js
@@ -6,16 +6,17 @@ import axios from 'axios'
 const bgmUser = process.env.BANGUMI_USER || 'geekaven'
 const bgmUrl = 'https://api.bgm.tv'
 const subjectType = {
-  'anime': 2,
+  anime: 2,
 }
 const collectionType = {
-  'want': 1,       //想看
-  'watched': 2,       //看过
-  'watching': 3,       //在看
+  want: 1, //想看
+  watched: 2, //看过
+  watching: 3, //在看
 }
 
 const headers = {
-  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+  'User-Agent':
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36',
 }
 
 async function fetchCollection(limit, offset) {
@@ -26,9 +27,9 @@ async function fetchCollection(limit, offset) {
       params: {
         subject_type: subjectType['anime'],
         limit: limit,
-        offset: offset
+        offset: offset,
       },
-      headers: headers
+      headers: headers,
     })
     return data
   } catch (error) {
@@ -38,7 +39,6 @@ async function fetchCollection(limit, offset) {
 }
 
 async function getCollectionMap() {
-
   const collection = []
   let limit = 100
 
@@ -55,7 +55,9 @@ async function getCollectionMap() {
 
   const totalPage = Math.ceil(total / limit)
   for (let page = 0; page < totalPage; page++) {
-    console.log(`- [INFO] Fetch ${bgmUser} collection page ${page}/${totalPage}`)
+    console.log(
+      `- [INFO] Fetch ${bgmUser} collection page ${page}/${totalPage}`
+    )
     const { data } = await fetchCollection(limit, page * limit)
     if (data) {
       collection.push(...data)
@@ -63,9 +65,13 @@ async function getCollectionMap() {
   }
 
   const collectionMap = {
-    'want': collection.filter(item => item['type'] === collectionType['want']),
-    'watched': collection.filter(item => item['type'] === collectionType['watched']),
-    'watching': collection.filter(item => item['type'] === collectionType['watching'])
+    want: collection.filter((item) => item['type'] === collectionType['want']),
+    watched: collection.filter(
+      (item) => item['type'] === collectionType['watched']
+    ),
+    watching: collection.filter(
+      (item) => item['type'] === collectionType['watching']
+    ),
   }
 
   return collectionMap
@@ -77,15 +83,22 @@ async function buildSubject() {
 
   for (const key in collectionMap) {
     if (Object.hasOwnProperty.call(collectionMap, key)) {
-      const data = collectionMap[key];
+      const data = collectionMap[key]
       const newData = []
       for (let i = 0; i < data.length; i++) {
-        const item = data[i];
+        const item = data[i]
         const subjectId = parseInt(item['subject_id'])
-        delete data[i].subject;
+        delete data[i].subject
         try {
-          console.log(`- [INFO] Fetch ${key} - ${subjectId}. ${i}/${data.length}`)
-          const { data: subject } = await axios.get(`https://cdn.jsdelivr.net/gh/geekaven/BangumiTV-Subject@latest/data/${Math.floor(subjectId / 100)}/${subjectId}.json`, { headers: headers })
+          console.log(
+            `- [INFO] Fetch ${key} - ${subjectId}. ${i}/${data.length}`
+          )
+          const { data: subject } = await axios.get(
+            `https://cdn.jsdelivr.net/gh/geekaven/BangumiTV-Subject@latest/data/${Math.floor(
+              subjectId / 100
+            )}/${subjectId}.json`,
+            { headers: headers }
+          )
           item['date'] = subject['date']
           item['images'] = subject['images']
           item['name'] = subject['name']
@@ -95,7 +108,9 @@ async function buildSubject() {
           item['eps'] = subject['eps']
           newData.push(item)
         } catch (error) {
-          console.log(`- [Error] ${key} - ${subjectId}. ${i}/${data.length}. ${error}`)
+          console.log(
+            `- [Error] ${key} - ${subjectId}. ${i}/${data.length}. ${error}`
+          )
         }
       }
 
@@ -105,11 +120,56 @@ async function buildSubject() {
         mkdirSync(dirName, { recursive: true })
       }
       console.log(`- [INFO] Write ${key} to ${filePath}`)
-      writeFileSync(filePath, JSON.stringify({ data: newData, total: newData.length }))
+      writeFileSync(
+        filePath,
+        JSON.stringify({ data: newData, total: newData.length })
+      )
     }
   }
 
   console.log('- [INFO] Build Subject done.')
 }
 
+// 每日放送
+async function fetchCalendar() {
+  const url = `${bgmUrl}/calendar`
+
+  try {
+    const { data } = await axios.get(url, {
+      headers: headers,
+    })
+
+    return data
+  } catch (error) {
+    console.log(`- [ERROR] Fetch ${url}. error: ${error}`)
+  }
+  return false
+}
+
+async function buildCalendar() {
+  const data = await fetchCalendar()
+
+  data.map((d) => {
+    return {
+      weekday: d['weekday'],
+      items: d['items'].map((item) => {
+        delete item['collection']
+        delete item['rating']
+        return item
+      }),
+    }
+  })
+
+  const filePath = `./data/calendar.json`
+  const dirName = dirname(filePath)
+  if (!existsSync(dirName)) {
+    mkdirSync(dirName, { recursive: true })
+  }
+  console.log(`- [INFO] Write Calendar to ${filePath}`)
+  writeFileSync(filePath, JSON.stringify(data))
+
+  console.log('- [INFO] Build Calendar done.')
+}
+
+await buildCalendar()
 await buildSubject()
